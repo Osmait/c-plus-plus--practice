bc6a5415fb9628ec3d4176024df6edad93d9001c
Saul Burgos <saulburgos6@gmail.com>
2022-12-31 17:13:36 -0400

    algoritmos
    Signed-off-by: Saul Burgos <saulburgos6@gmail.com>

---
+++ algoritmos/sorting/bubble_sort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n;
+    bool swap_check = true;
+    std::cout << "Enter the amount of numbers to sort: ";
+    std::cin >> n;
+    std::vector<int> numbers;
+    std::cout << "Enter " << n << " numbers: ";
+    int num;
+
+    // Input
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> num;
+        numbers.push_back(num);
+    }
+
+    // Bubble sorting
+    for (int i = 0; (i < n) && (swap_check); i++)
+    {
+        swap_check = false;
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (numbers[j] > numbers[j + i])
+            {
+                swap_check = true;
+                std::swap(numbers[j], numbers[j + 1]);
+            }
+        }
+    }
+
+    // Output
+
+    std::cout << "\nSorted Array : ";
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (i != numbers.size() - 1)
+        {
+            std::cout << numbers[i] << ", ";
+        }
+        else
+        {
+            std::cout << numbers[i] << std::endl;
+        }
+    }
+}
